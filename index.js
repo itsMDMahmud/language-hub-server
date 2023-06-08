28fa047117c9584c5d8cb685fb32cb4611f95473
itsMDMahmud <itsmdmahmud@gmail.com>
2023-06-08 19:59:50 +0600

    stored data in mongo done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();('mongodb');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,9 +12,6 @@ app.use(express.json());
 
 // const usersCollection = client.db("languageDb").collection('users');
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.reyfrcm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,20 +28,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("languageDb").collection("users");
 
-    const usersCollection = client.db('languageDb').collection('users');
+    app.get("/users", async (req, res) => {
+      // const user = req.body;
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    // app.get("/allusers", async (req, res) => {
+    //   // const user = req.body;
+    //   const result = await usersCollection.find().toArray();
+    //   res.send(result);
+    // });
 
-
-    app.get('/users', async(req, res) => {
-        // const user = req.body;
-        const result = await usersCollection.find().toArray();
-        res.send(result);
-    })
-
+    app.post("/users", async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  const existingUser = await usersCollection.findOne(query);
+  console.log("existingUser", existingUser);
+  if (existingUser) {
+    return res.send({ message: "user already exist" });
+  }
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,18 +67,12 @@ run().catch(console.dir);
 
 
 
-//users related APIs
-app.post('/users', async(req, res) => {
-    const user = req.body;
-    const result = await usersCollection.insertOne(user);
-    res.body(result);
-})
 
 
-app.get ('/', (req, res) => {
-    res.send('Language hub server is running');
-})
+app.get("/", (req, res) => {
+  res.send("Language hub server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Language hub server is running on port: ${port}`)
-})
+  console.log(`Language hub server is running on port: ${port}`);
+});
