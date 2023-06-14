54a8a4f86f2ab95d06b390861f1f5dd6690d9425
itsMDMahmud <itsmdmahmud@gmail.com>
2023-06-14 13:17:20 +0600

    changes

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+    app.get("/payments", async (req, res) => {
+      // const user = req.body;
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    });
     
 
 
